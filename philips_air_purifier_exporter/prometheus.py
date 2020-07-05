d770477544bb43d74fd1a6b1836bee166ed7956f
Harri Hämäläinen <hhamalai@iki.fi>
2020-07-05 12:26:58 +0300

    Export more reported metrics

--- philips_air_purifier_exporter/prometheus.py
+++ philips_air_purifier_exporter/prometheus.py
@@ -8,7 +8,13 @@ def to_metrics(status):
         air_quality {status["iaql"]}
         is_manual {1 if is_manual_mode(status) else 0}
         is_on {1 if is_on(status) else 0}
+        temp {status["temp"]}
         pm25 {status["pm25"]}
+        is_purifying {1 if "P" in status["func"] else 0}
+        is_humidifying {1 if "H" in status["func"] else 0}
+        humidity {status["rh"]}
+        target_humidity {status["rhset"]}
         speed {fan_speed_to_int(status)}
+        water_level {status["wl"]}
     """
     ).lstrip()
